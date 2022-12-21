d9b633fe433d292a5f2d06283298b66da65df6a3
Sander <sander@verbruggen.ws>
2022-12-21 20:23:12 +0100

    Day 8 part 1

---
+++ src/main/kotlin/com/sanderverbruggen/aoc2022_kotlin/Day08TreetopTreeHouse.kt
@@ -0,0 +1,56 @@
+package com.sanderverbruggen.aoc2022_kotlin
+
+class TreetopTreeHouse {
+}
+
+class Forest(lines: List<String>) {
+    val maxX = lines[0].length - 1
+    val maxY = lines.size - 1
+    val forest: Array<Array<Int>> = lines
+        .map { line ->
+            line
+                .map { it.digitToInt() }
+                .toTypedArray()
+        }
+        .toTypedArray()
+
+    fun countVisibleTrees(): Int {
+        return forest.mapIndexed { y, row ->
+            row
+                .mapIndexed { x, _ -> isTreeVisible(x, y) }
+                .filter { it == true }
+                .count()
+        }.sum()
+    }
+    
+    fun isTreeVisible(x: Int, y: Int): Boolean {
+        return isBorder(x, y) || isTreeVisibleHor(x, y) || isTreeVisibleVer(x, y)
+    }
+
+    private fun isBorder(x: Int, y: Int) = x == 0 || y == 0 || x == maxX || y == maxY
+
+    private fun isTreeVisibleHor(x: Int, y: Int): Boolean {
+        val treeLine = forest[y]
+        return isVisible(treeLine, x);
+    }
+
+    private fun isTreeVisibleVer(x: Int, y: Int): Boolean {
+        val treeLine = forest
+            .map { it[x] }
+            .toTypedArray()
+        return isVisible(treeLine, y)
+    }
+
+    private fun isVisible(treeLine: Array<Int>, pos: Int): Boolean {
+        val treeHeight = treeLine[pos]
+        var hidden = treeLine.take(pos).filter { it >= treeHeight }.any()
+        hidden = hidden && treeLine.drop(pos + 1).filter { it >= treeHeight }.any()
+        return !hidden
+    }
+}
+
+
+fun main() {
+    val forest = Forest(readFile("day8_puzzle.txt").lines())
+    println(forest.countVisibleTrees())
+}
