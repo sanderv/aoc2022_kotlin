f5b7220555b7ec15c5c03bc2632a4a2bcba3678a
Sander <sander@verbruggen.ws>
2022-12-29 10:49:03 +0100

    Day 8 part 2

--- src/main/kotlin/com/sanderverbruggen/aoc2022_kotlin/Day08TreetopTreeHouse.kt
+++ src/main/kotlin/com/sanderverbruggen/aoc2022_kotlin/Day08TreetopTreeHouse.kt
@@ -15,15 +15,14 @@ class Forest(lines: List<String>) {
         .toTypedArray()
 
     fun countVisibleTrees(): Int {
-        return forest.mapIndexed { y, row ->
+        return forest.flatMapIndexed { y, row ->
             row
                 .mapIndexed { x, _ -> isTreeVisible(x, y) }
-                .filter { it == true }
-                .count()
-        }.sum()
+        }.count { it }
+
     }
-    
-    fun isTreeVisible(x: Int, y: Int): Boolean {
+
+    private fun isTreeVisible(x: Int, y: Int): Boolean {
         return isBorder(x, y) || isTreeVisibleHor(x, y) || isTreeVisibleVer(x, y)
     }
 
@@ -47,10 +46,50 @@ class Forest(lines: List<String>) {
         hidden = hidden && treeLine.drop(pos + 1).filter { it >= treeHeight }.any()
         return !hidden
     }
+
+    fun findMaxScenicTree(): Int {
+        return forest.flatMapIndexed() { y, row ->
+            row
+                .mapIndexed { x, _ -> scenicScore(x, y) }
+        }.max()
+    }
+
+    private fun scenicScore(x: Int, y: Int): Int {
+        val scoreNorth = scenicScore(x, y, dy = -1)
+        val scoreSouth = scenicScore(x, y, dy = 1)
+        val scoreEast = scenicScore(x, y, dx = 1)
+        val scoreWest = scenicScore(x, y, dx = -1)
+        val score = scoreNorth * scoreSouth * scoreEast * scoreWest
+        println("\t($x, $y) = $score")
+        return score
+    }
+
+    private fun scenicScore(x: Int, y: Int, dx: Int = 0, dy: Int = 0): Int {
+        if (isBorder(x, y))
+            return 0;
+        var score = 0
+        val viewHeight = forest[y][x]
+        var currentX = x + dx;
+        var currentY = y + dy;
+        while (inForest(currentX, currentY)) {
+            score++
+            if (forest[currentY][currentX] >= viewHeight) {
+                return score
+            }
+            currentX += dx
+            currentY += dy
+        }
+//        println("($x, $y) = $score")
+        return score
+    }
+
+    private fun inForest(x: Int, y: Int): Boolean = x >= 0 && y >= 0 && x <= maxX && y <= maxY
 }
 
 
 fun main() {
+//    val forest = Forest(readFile("day8_example.txt").lines())
     val forest = Forest(readFile("day8_puzzle.txt").lines())
     println(forest.countVisibleTrees())
+    println(forest.findMaxScenicTree())
 }
