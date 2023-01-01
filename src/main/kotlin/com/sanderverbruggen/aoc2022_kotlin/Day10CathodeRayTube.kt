9fe42f064e6d9ceac763bb8f0d30c6facaaf3749
Sander <sander@verbruggen.ws>
2023-01-01 16:40:15 +0100

    Day 10 part 2

--- src/main/kotlin/com/sanderverbruggen/aoc2022_kotlin/Day10CathodeRayTube.kt
+++ src/main/kotlin/com/sanderverbruggen/aoc2022_kotlin/Day10CathodeRayTube.kt
@@ -1,44 +1,83 @@
 package com.sanderverbruggen.aoc2022_kotlin
 
-class CathodeRayTube {
-    private val cpu = CPU(1)
-    fun solve(lines: List<String>): Int {
-        var pointer = 0
-        val signalStrength = mutableListOf<Int>(1)
 
-        while (pointer < lines.size) {
-            val parts = lines[pointer].split(" ")
-            val opcode = parts.first()
-            val param = if (parts.size > 1) parts.last().toInt() else 0
+class CathodeRayTube(lines: List<String>) {
+    private val cpu = CPU(1, lines)
+    private var screen = ""
+    private var cycle = 0
 
-            val instr = CRInstruction.valueOf(opcode.uppercase())
-            repeat(instr.cycles) {
-                signalStrength.add(cpu.regX)
-                println("${signalStrength.size}: ${cpu.regX}")
+    fun solve(): String {
+        var busyCycles = 0
+        var instruction: CRInstruction? = null
+
+        while (true) {
+            if (busyCycles == 0) {
+                instruction = cpu.getNextInstruction()
+                println(instruction)
+                if (instruction == null) {
+                    return screen
+                }
+                busyCycles = instruction.cycles
             }
-            if (instr == CRInstruction.ADDX) {
-                cpu.regX = cpu.regX + param
-                println("$pointer: add $param -> ${cpu.regX}")
+
+            updateScreen()
+            cycle++
+            busyCycles--
+            if (busyCycles == 0) {
+                instruction?.let { execute(it) }
             }
-            pointer++
         }
+    }
 
-        return listOf(20, 60, 100, 140, 180, 220)
-            .map { i -> i * signalStrength[i] }
-            .also { println(it) }
-            .sum()
+    private fun updateScreen() {
+        var rayPos = cycle % 40
+        if (cpu.regX >= (rayPos - 1) && cpu.regX <= (rayPos + 1)) {
+            screen += "#"
+        } else {
+            screen += "."
+        }
+    }
+
+    private fun execute(instruction: CRInstruction) {
+        if (instruction == CRInstruction.ADDX) {
+            cpu.regX = cpu.regX + instruction.param
+        }
     }
 }
 
-private class CPU(var regX: Int)
+private class CPU(var regX: Int, private val lines: List<String>) {
+    private var pointer = 0
+
+    fun getNextInstruction(): CRInstruction? {
+        if (pointer < lines.size) {
+            val parts = lines[pointer].split(" ")
+
+            val instruction = CRInstruction.valueOf(parts.first().uppercase())
+            instruction.param = if (parts.size > 1) parts.last().toInt() else 0
+            pointer++
+            return instruction
+        } else {
+            return null
+        }
+    }
+}
 
 enum class CRInstruction(val cycles: Int) {
     NOOP(1),
-    ADDX(2)
+    ADDX(2);
+
+    var param: Int = 0
+
+    override fun toString(): String {
+        return "${this.name} " + if (this == ADDX) param.toString() else ""
+    }
 }
 
 fun main() {
 //    val lines = readFile("day10_example.txt").lines()
     val lines = readFile("day10_puzzle.txt").lines()
-    println(CathodeRayTube().solve(lines))
+    val screen = CathodeRayTube(lines).solve()
+        .chunked(40)
+        .joinToString("\n")
+    println(screen)
 }
